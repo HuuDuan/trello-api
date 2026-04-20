ab52acc53a2082de35f1925504dd7aafdb9a66ab
HuuDuan <duannguyenhuu04@gmail.com>
2026-04-21 00:19:17 +0700

    Update Cors not allow localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 import { env } from '~/config/environment'
 // NHững domain được phép truy cập tới tài nguyên của server
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173'
+  // 'http://localhost:5173'
 ]
 
 export const BOARD_TYPE = {
